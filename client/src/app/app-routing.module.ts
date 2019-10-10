e62c2b50e44cf305379ae8685765eb9c3552e738
ArtG <artem.girda@gmail.com>
2019-10-10 16:47:38 +0300

    init ng ang start logic router and layout

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+
+
+const routes: Routes = [
+  {
+    path: '', 
+    component: AuthLayoutComponent, 
+    children: [
+      {path: 'login', component: LoginPageComponent}
+    ]
+  },
+  {
+    path: '', 
+    component: SiteLayoutComponent, 
+    children: [
+      
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
