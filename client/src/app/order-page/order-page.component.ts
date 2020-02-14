2a657e19716c6e4a3768720e5131fa5f41618a4b
ArtG <artem.girda@gmail.com>
2020-02-14 13:35:07 +0200

    finish order-page and logic in progress history-page

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import {MaterialInstance, MaterialService} from "../shared/classes/material.service";
+import {OrderService} from "./order.service";
+import {Order, OrderPosition} from "../shared/interfaces";
+import {OrdersServices} from "../shared/services/orders.services";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.css']
+  styleUrls: ['./order-page.component.css'],
+  providers: [OrderService]
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('modal', {static: false}) modalRef: ElementRef;
+  modal: MaterialInstance;
+  oSub: Subscription;
+  isRoot: boolean;
+  pending = false;
+
+  constructor(private router: Router,
+              private order: OrderService,
+              private ordersService: OrdersServices) { }
 
   ngOnInit() {
+    this.isRoot = this.router.url === '/order';
+    this.router.events.subscribe(event => {
+      if (event instanceof  NavigationEnd) {
+        this.isRoot = this.router.url === '/order';
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.modal.destroy();
+    if (this.oSub) {
+      this.oSub.unsubscribe();
+    }
+  }
+
+  ngAfterViewInit() {
+    this.modal = MaterialService.initModal(this.modalRef)
+  }
+
+  removePosition(orderPosition: OrderPosition) {
+    this.order.remove(orderPosition);
+    MaterialService.toast(`Позиция ${orderPosition.name} удалена.`)
+  }
+
+  openModal() {
+    this.modal.open()
+  }
+
+  cancelModal() {
+    this.modal.close()
+  }
+
+  submitModal() {
+    this.pending = true;
+    const order: Order = {
+      list: this.order.list.map(item => {
+        delete item._id;
+        return item;
+      })
+    };
+
+    this.oSub = this.ordersService.create(order).subscribe(
+      newOrder => {
+        MaterialService.toast(`Заказ №${newOrder.order} был добавлен.`)
+        this.order.clear();
+      },
+      error => MaterialService.toast(error.error.message),
+      () => {
+        this.modal.close();
+        this.pending = false;
+      }
+    )
   }
 
 }
